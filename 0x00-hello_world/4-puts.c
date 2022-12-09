a59f653d75840f13b6f5dbfe4bb2bd1f8677ab75
chris <christ4sure08@yahoo.com>
2022-12-08 20:16:00 -0800

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+int main(){
+	// initializing the string
+	char string[] = "Programming is like building a multilingual puzzle";
+	// writing our string to stdout
+	puts(string);
+	return 0;
+}
+

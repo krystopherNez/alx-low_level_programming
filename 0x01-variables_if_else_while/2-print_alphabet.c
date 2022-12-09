0f801ec9abcd9c02d5c70da12328454b1e8d7839
chris <christ4sure08@yahoo.com>
2022-12-09 11:44:43 -0800

    write abc with for loop in c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int x;
+	for(x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+
+	return (0);
+}

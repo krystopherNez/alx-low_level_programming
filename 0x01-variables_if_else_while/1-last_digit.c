a5bf9df5226d35955314be21786ee269b5ee6099
chris <christ4sure08@yahoo.com>
2022-12-09 19:26:20 -0800

    betty styling issues

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,16 +3,11 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* 
-* Description: 'the program's description'
-* 
-* Return: Always 0 (Success)
-*/
-
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;

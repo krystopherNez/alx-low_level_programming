b959aa77706a1235b9d0c6a4b3eee9b912c04833
chris <christ4sure08@yahoo.com>
2022-12-09 19:29:09 -0800

    fixing betty style issues

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
-#include <ctype.h>
-/**
-* main - Entry point
-*
-* Description: 'the program's description'
-*
-* Return: Always 0 (Success)
-*/
 
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int x;
+	char letter;
 
-	for (x = 'a'; x <= 'z'; x++)
-	{
-		putchar(x);
-	}
-	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
 
 	return (0);

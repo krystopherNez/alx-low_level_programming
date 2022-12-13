e37faa5e24b866237a73ec76c6bf54984bb4faed
chris <christ4sure08@yahoo.com>
2022-12-12 18:50:49 -0800

    put function in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 # include<stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char string[] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(string);
+
 	return (0);
 }
 
